2969d8d5d76597d82ae994f22c438ab3f59edfa3
Dyson\Marcos <marcosdecastrolimeira@gmail.com>
2018-06-04 22:02:26 -0300

    Tabela da classe Cliente

--- build/classes/br/senac/LojaEletronicos/DB/CreationScript.sql
+++ build/classes/br/senac/LojaEletronicos/DB/CreationScript.sql
@@ -6,8 +6,19 @@ CREATE TABLE Cliente (
     nome varchar(256) not null,
     dataDeNascimento date,
     sexo varchar(10),
-    CPF varchar(14),
-    
+    CPF varchar(14) not null,
+    RG varchar(14),
+    estadoCivil varchar(20),
+    endereco varchar(256),
+    numero int,
+    CEP varchar(10),
+    bairro varchar(256),
+    complemento varchar(20),
+    cidade varchar(256),
+    telefoneFixo varchar(20),
+    celular varchar(20),
+    email varchar(128),
+    prefContato int,
     PRIMARY KEY (CodCliente)
 );
 
