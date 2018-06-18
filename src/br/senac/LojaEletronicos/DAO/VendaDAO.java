d03438258525859c7e9eda4b5c593ccdb06718cf
MARCOS DE CASTRO LIMEIRA <marcos.climeira@senacsp.edu.br>
2018-06-18 19:43:21 -0300

    Venda DAO 1

--- src/br/senac/LojaEletronicos/DAO/VendaDAO.java
+++ src/br/senac/LojaEletronicos/DAO/VendaDAO.java
@@ -17,6 +17,7 @@ import java.sql.Timestamp;
  * @author marcos.climeira
  */
 public class VendaDAO {
+    
     public static void inserirVenda(Venda v)
             throws SQLException, Exception {
         //Monta a string de inserção de um cliente no BD,
@@ -53,4 +54,45 @@ public class VendaDAO {
             }
         }
     }
+    
+  public static void atualizarProduto(Venda v) throws SQLException, Exception {
+        //Monta a string de atualização do cliente no BD, utilizando
+        //prepared statement
+        String sql = "UPDATE venda SET numeroDoPedido=?, dataVenda=?, idCliente=?, valoTotal=?"
+                + " WHERE numeroDoPedido = ?";
+        //Conexão para abertura e fechamento
+        
+        Connection connection = null;
+        //Statement para obtenção através da conexão, execução de
+        //comandos SQL e fechamentos
+        PreparedStatement preparedStatement = null;
+        try {
+            //Abre uma conexão com o banco de dados
+            connection = ConnectionUtils.getConnection();
+            //Cria um statement para execução de instruções SQL
+            preparedStatement = connection.prepareStatement(sql);
+            //Configura os parâmetros do "PreparedStatement"
+            preparedStatement.setInt(1, v.getNumeroDoPedido());
+            Timestamp t = new Timestamp(v.getDataVenda().getTime());
+            preparedStatement.setTimestamp(2, t);
+            preparedStatement.setInt(3, v.getIdCliente());
+            preparedStatement.setFloat(4, v.getValorTotal());
+            preparedStatement.setInt(5, v.getNumeroDoPedido());
+
+
+
+            //Executa o comando no banco de dados
+            preparedStatement.execute();
+        } finally {
+            //Se o statement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+    
 }
